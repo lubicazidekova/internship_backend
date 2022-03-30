0270bf23095049334a4e93a34ec264b8bcfc7764
lubicazidekova <zidekova16@stud.uniza.sk>
2022-03-30 18:55:20 +0200

    Entrypoint

---
+++ src/api/v1/patients/get.patients.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express'
+
+
+
+
+export const workflow = (req: Request, res: Response) => {
+
+    res.json({
+        patients: [{
+            id: 1,
+            name: "john doe"
+        }]
+    })
+}
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import v1 from './api/v1'
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+// Register router
+app.use('/api/v1', v1())
+
+export default app
