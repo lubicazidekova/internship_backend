70f4c308e1e6cb4c2547237162cc3f0d9ee71c65
lubicazidekova <zidekova16@stud.uniza.sk>
2022-04-25 17:37:05 +0200

    index update

--- src/api/v1/patients/index.ts
+++ src/api/v1/patients/index.ts
@@ -1,12 +1,22 @@
 import { Router } from 'express'
-
 import validationMiddleware from '../../../middlewares/validationMiddleware'
+
+
 import * as GetPatients from './get.patients'
+import * as PostPatients from './post.patient'
+import * as GetPatient from './get.patient'
+import * as PatchPatient from './patch.patient'
+import * as DeletePatient from './delete.patient'
+
 
 const router = Router()
 
 export default () => {
-    router.get('/', validationMiddleware() , GetPatients.workflow)
+    router.get('/',validationMiddleware(GetPatients.requestSchema), GetPatients.workflow)
+    router.get('/:patientID',GetPatient.workflow)
+    router.patch('/:patientID',PatchPatient.workflow)
+    router.post('/:patientID',PostPatients.workflow)
+    router.delete('/:patientID',DeletePatient.workflow)
 
     return router
 }
