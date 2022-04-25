38ffcd9fd9b3e488575f1b62024adda314a36ee7
lubicazidekova <zidekova16@stud.uniza.sk>
2022-04-25 16:09:26 +0200

    database update

--- config/database.ts
+++ config/database.ts
@@ -1,6 +1,6 @@
 import { Options } from 'sequelize'
 
-// eslint-disable-next-line import/prefer-default-export
+//eslint-disable-next-line import/prefer-default-export
 export const development = {
 	url: process.env.POSTGRESQL_URL,
 	options: <Options>{
